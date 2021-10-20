2cebd35a46438d6f6655c8bf9a64a0a8d4f3dd08
sapta <sbasu2324>
2021-10-20 18:10:38 +0530

    Added i18n

--- src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,14 +1,28 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	MessageSource messageSource;
 
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
-	public HelloWorldBean HelloWorld() {
-		return new HelloWorldBean("Hello World ..!");
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-bean")
+	public HelloWorldBean helloWorld() {
+		return new HelloWorldBean("Hello World Bean..!");
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("greetings.message", null, "Default Greetings", LocaleContextHolder.getLocale());
 	}
 }
